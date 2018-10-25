04d6d275d29868e414330463a44a1aed657bd79c
rbaluta17dw <rbaluta17dw@ikzubirimanteo.com>
2018-10-25 11:03:08 +0200

    bootstrap

--- resources/views/bienvenida.blade.php
+++ resources/views/bienvenida.blade.php
@@ -1,36 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+	<title>Calculadora</title>
+	<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
+	<div class="container">
+		<h1>Reto calculadora</h1>
+		<form action="mcalculadora" method="POST">
 
-<h1>Reto calculadora</h1>
-<form action="mcalculadora" method="POST">
-
-     @csrf
-  <label for="Nombre">Introduce tu nombre</label>
-  <input type="text" name="nombre">
-  <p>El nombre introducido aparecera en la calculadora</p>
-  <br>
-  
-  
-  
-
-  <input type="submit" value="Enviar">
-</form>
+			@csrf
+			<label for="Nombre">Introduce tu nombre</label>
+			<input type="text" name="nombre">
+			<p>El nombre introducido aparecera en la calculadora</p>
+			<br>
 
 
 
 
+			<input type="submit" value="Enviar">
+		</form>
 
-
-
-
-
-
-
-
+	</div>
 
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
+        <link href="" rel="stylesheet" type="text/css">
         <!-- Styles -->
         <style>
             html, body {
